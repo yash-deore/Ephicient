7560d5182822dfab46d2dcc402c67d5669f82146
yash-deore <codemaninfinity17@gmail.com>
2022-03-13 10:24:22 +0530

    Update PatientDashboard.js

--- frontend/src/Pages/PatientDashboard.js
+++ frontend/src/Pages/PatientDashboard.js
@@ -1,23 +1,48 @@
 import { useState, useEffect } from "react";
-import { Card, Container } from "react-bootstrap";
+import { Card, Container, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const PatientDashboard = () => {
     const navigate = useNavigate();
 
     const [age, setAge] = useState(0);
     const [gender, setGender] = useState("");
+    const [description, setDescription] = useState("");
     const [phoneNumber, setPhoneNumber] = useState(0);
     const [isPregnant, setIsPregnant] = useState("no");
     const [symptoms, setSymptoms] = useState("");
     const [allergies, setAllergies] = useState("");
     const [currentMedications, setCurrentMedications] = useState("");
+    const [success, setSuccess] = useState(false);
 
     const pregnancyStatus = () => {
         if (gender === "female")
             if (isPregnant === "no")
                 return <Card.Title>Not Pregnant</Card.Title>;
             else return <Card.Title>Pregnant</Card.Title>;
+    };
+
+    const confirmPatientRequest = () => {
+        axios
+            .post(
+                "https://damp-fjord-26709.herokuapp.com/submitting_the_form",
+                {
+                    age: age,
+                    gender: gender,
+                    allergy: allergies,
+                    current_medications: currentMedications,
+                    phone_number: phoneNumber,
+                    pregnant: isPregnant,
+                    description: description,
+                    symptom: symptoms,
+                    location: "Queue_BBBBB",
+                }
+            )
+            .then((response) => {
+                console.log(response, "success");
+                setSuccess(true);
+            });
     };
 
     useEffect(() => {
@@ -30,6 +55,7 @@ const PatientDashboard = () => {
         } else {
             setAge(localStorage.getItem("age"));
             setPhoneNumber(localStorage.getItem("phoneNumber"));
+            setDescription(localStorage.getItem("description"));
             setGender(localStorage.getItem("gender"));
             if (gender === "female")
                 if (localStorage.getItem("isPregnant") === "yes")
@@ -44,7 +70,7 @@ const PatientDashboard = () => {
     return (
         <Container>
             <h1>😄 Welcome 😄</h1>
-            <Card style={{ width: "18rem" }}>
+            <Card>
                 <Card.Body>
                     <Card.Title>
                         {age} years, {gender}
@@ -56,6 +82,10 @@ const PatientDashboard = () => {
                         <b>Phone Number:</b> {phoneNumber}
                     </Card.Text>
                     <hr />
+
+                    <Card.Text>
+                        <b>Description:</b> {description}
+                    </Card.Text>
 
                     <Card.Text>
                         <b>Symptoms:</b> {symptoms}
@@ -72,10 +102,20 @@ const PatientDashboard = () => {
                             ? "None"
                             : currentMedications}
                     </Card.Text>
+
+                    {success ? (
+                        <h4>Request Submitted 😃</h4>
+                    ) : (
+                        <Button onClick={confirmPatientRequest()}>
+                            Confirm Request
+                        </Button>
+                    )}
+
+                    <Button onClick={confirmPatientRequest()}>
+                        Confirm Request
+                    </Button>
                 </Card.Body>
             </Card>
-            Your request is under process <br />
-            Patient Id:
         </Container>
     );
 };
